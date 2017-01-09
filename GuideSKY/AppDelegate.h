a46f28c0399a06f9a6f07052ea12abc22696c53a
xiejiabao <13140521434@163.com>
2017-01-09 11:16:24 +0800

    Initial Commit

---
+++ GuideSKY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuideSKY
+//
+//  Created by yijiaanhao on 2017/1/9.
+//  Copyright © 2017年 谢家宝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuideSKY/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GuideSKY
+//
+//  Created by yijiaanhao on 2017/1/9.
+//  Copyright © 2017年 谢家宝. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
